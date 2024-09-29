0915147c89231963c2e40b5f62918353cd81d265
gayratovdv <javohirgayratov26@gmail.com>
2024-09-29 09:11:26 +0500

    add

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<h1>Home</h1>} />
         <Route path="/about" element={<h1>About</h1>} />
+        <Route path="/contact" element={<h1>Contact</h1>} />
       </Routes>
     </>
   );
